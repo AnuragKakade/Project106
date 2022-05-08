c30fe859d7d2c2b1c6ea4c87432bc646c0a95a96
AnuragKakade <86247136+AnuragKakade@users.noreply.github.com>
2022-05-08 13:05:17 +0530

    Add files via upload

---
+++ project106b.py
@@ -0,0 +1,35 @@
+import csv
+import numpy as np
+import plotly.express as px
+
+
+def plotfigure(data_path):
+    with open(data_path) as csv_file:
+        df=csv.DictReader(csv_file)
+        fig=px.scatter(df,x="Coffee in ml",y="sleep in hours")
+        fig.show()
+
+def getDataSource(data_path):
+    coffee_in_ml=[]
+    sleep_in_hours=[]
+    with open(data_path) as csv_file:
+        csv_reader=csv.DictReader(csv_file)
+        for row in csv_reader:
+            coffee_in_ml.append(float(row["Coffee in ml"]))
+            sleep_in_hours.append(float(row["sleep in hours"]))
+    return{"x":coffee_in_ml,"y":sleep_in_hours}  
+    
+#append used to add vlues to the list
+#float is for decimal points
+
+def findCorrelation(dataSource):
+    correlation=np.corrcoef(dataSource["x"],dataSource["y"])
+    print("correlation between ",correlation[0,1])
+
+def setup():
+    data_path="cups of coffee vs hours of sleep.csv"
+    dataSource=getDataSource(data_path)
+    findCorrelation(dataSource)
+    plotfigure(data_path)
+    
+setup()
